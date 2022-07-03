d4cda95101311b66049879f6202053050e2f34dc
Budi Santoso <103356160+budisdjung@users.noreply.github.com>
2022-07-03 20:23:18 +0700

    Create endpoint for CRUD

--- routes/pet.js
+++ routes/pet.js
@@ -1,47 +1,31 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../config/db");
-const ControllerUsers = require('../controllers/controllerUsers')
-const jwt = require('jsonwebtoken')
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+
 
 router.post('/', function (req, res, next) {
-  if (!req.headers || !req.headers.authorization) {
-    res.status(401).json({ message: 'Unauthorized'})
+  const petstore = JSON.parse(fs.readFileSync('petstore.json', 'utf-8'))
+  petstore.push(req.body)
+  fs.writeFileSync('products.json', JSON.stringify(petstore, null, 2))
+  res.status(201).json(req.body)
+})
+
+router.get('/:id', function (req, res, next) {
+  const petstore = JSON.parse(fs.readFileSync('petstore.json', 'utf-8'))
+  const params = req.params.id
+  if (petstore[params]) {
+      res.status(200).json(petstore[params])
   }
-
-  const token = req.headers.authorization
-  const verifyToken = jwt.verify(token, 'shhhhh')
-  
-  if (verifyToken) {
-    next()
+  else {
+      res.status(404).json({message: 'Data Not Found'})
   }
-  res.status(401).json({ message: 'Unauthorized'})
-}, ControllerUsers.createUser)
-
-router.post('/login', ControllerUsers.login)
-
-router.post("/", function (req, res, next) {
-  const body = req.body;
-  db.run(query, [body.id, body.name], function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.status(201).json({ message: "Success create new data!" });
-});
-
-router.get("/", function (req, res, next) {
-  const query = "SELECT * FROM users";
-  db.all(query, (err, row) => {
-    res.status(200).json(row);
-  });
-});
+})
 
 router.put("/:id", function (req, res, next) {
-  const query = "UPDATE users SET name = ? WHERE id =?";
-  const userId = req.params.id;
+  const query = "UPDATE pet SET name = ? WHERE id =?";
+  const petId = req.params.id;
   const body = req.body;
-  db.run(query, [body.name, userId], function (err) {
+  db.run(query, [body.name, petId], function (err) {
     if (err) {
       console.log(err);
     }
@@ -50,10 +34,10 @@ router.put("/:id", function (req, res, next) {
 });
 
 router.delete("/:id", function (req, res, next) {
-  const query = "DELETE from users WHERE id = ?";
-  const userId = req.params.id;
+  const query = "DELETE from pet WHERE id = ?";
+  const petId = req.params.id;
 
-  db.run(query, [userId], function (err) {
+  db.run(query, [petId], function (err) {
     if (err) {
       console.log(err);
     }
@@ -61,4 +45,4 @@ router.delete("/:id", function (req, res, next) {
   res.status(200).json({ message: "Success delete new data!" });
 });
 
-module.exports = router;
+module.exports = router
